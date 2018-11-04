f5fb7f453221a49e287fed57119204a99b3f0e0b
hasitha <hasithadkr7@gmail.com>
2018-11-04 20:43:29 +0530

    PeerNode changes

---
+++ src/StartNode1.java
@@ -0,0 +1,8 @@
+public class StartNode1 {
+    public static void main(String[] args) {
+        PeerNode peerNode = new PeerNode("127.0.0.1",56551,"node1");
+        //PeerNode peerNode1 = new PeerNode("127.0.0.1",32212, "node2");
+//        peerNode.start();
+        //peerNode1.getRountingTable();
+    }
+}
---
+++ src/StartNode2.java
@@ -0,0 +1,8 @@
+public class StartNode2 {
+    public static void main(String[] args) {
+        //PeerNode peerNode = new PeerNode("127.0.0.1",56551,"node1");
+        PeerNode peerNode1 = new PeerNode("127.0.0.1",56552, "node2");
+        //peerNode.getRountingTable();
+//        peerNode1.start();
+    }
+}
